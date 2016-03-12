d7d56ea688af82efa191c48dbf8ef99efcecd5cb
stars_cj <stars_chenjiao@163.com>
2016-03-12 09:45:27 +0800

    学习廖教程前三章

---
+++ learn_liaopy/l1.py
@@ -0,0 +1,6 @@
+#encoding:utf8
+a=100
+if a>=0:
+    print a
+else:
+    print -a
---
+++ learn_liaopy/l2.py
@@ -0,0 +1,32 @@
+#encoding:utf8
+# age=int(raw_input('please input your age:'))
+# if age >=18:
+#     print 'your age is ',age
+#     print 'adult'
+# elif age >=6:
+#     print 'your age is ',age
+#     print 'teenager'
+# else:
+#     print 'kid'
+
+# names=['chen','jiao','bao','jian','hang']
+# for name in names:
+# 	print name
+
+# sum=0
+# for x in range(101):
+# 	sum=sum+x
+# print sum
+
+# sum=0
+# n=99
+# while n>0:
+# 	sum=sum+n
+# 	n=n-2
+# print sum
+
+birth=int(raw_input('birth:'))
+if birth<2000:
+    print '00前'
+else:
+    print '00后'
